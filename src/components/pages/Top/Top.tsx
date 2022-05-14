c2c982c3c0715ee2e81e3c80c77cbb747c7e8810
akaneburyo <akaneburyo@gmail.com>
2022-05-14 15:45:59 +0900

    update: add preview

--- src/components/pages/Top/Top.tsx
+++ src/components/pages/Top/Top.tsx
@@ -1,15 +1,16 @@
 import type { VFC } from 'react'
-import { useEffect } from 'react'
+import { useState, useCallback, useRef, useMemo } from 'react'
 
 // UI
-import { Container } from '@chakra-ui/react'
+import { HStack, Box, StackDivider } from '@chakra-ui/react'
 
 // yjs
 import * as Y from 'yjs'
 import { WebsocketProvider } from 'y-websocket'
 
 // Editor
-import { useEditor, EditorContent } from '@tiptap/react'
+import { EditorContent as Editor, useEditor } from '@tiptap/react'
+import type { PureEditorContent, EditorEvents } from '@tiptap/react'
 import DocumentExtension from '@tiptap/extension-document'
 import TextExtension from '@tiptap/extension-text'
 import CodeBlockLowlightExtension, {
@@ -21,8 +22,13 @@ import 'highlight.js/styles/ascetic.css'
 
 import '@/components/pages/Top/styles.css'
 
+// Preview
+import Preview from '@uiw/react-markdown-preview'
+
 // Constants
 import { WS } from '@/constants/config'
+
+const mdBodyIdentifier = 'md_body'
 
 const CustomCodeBlockExtension = CodeBlockLowlightExtension.extend<CodeBlockLowlightOptions>({
   content: 'text*',
@@ -31,7 +37,7 @@ const CustomCodeBlockExtension = CodeBlockLowlightExtension.extend<CodeBlockLowl
     languageClassPrefix: 'language-',
     exitOnTripleEnter: false,
     exitOnArrowDown: false,
-    HTMLAttributes: {},
+    HTMLAttributes: { id: mdBodyIdentifier },
     lowlight,
     defaultLanguage: null,
   }),
@@ -39,33 +45,54 @@ const CustomCodeBlockExtension = CodeBlockLowlightExtension.extend<CodeBlockLowl
 })
 
 const doc = new Y.Doc()
-const wsProvider = new WebsocketProvider(`${WS.HOST}`, 'editor', doc)
 
 export const Top: VFC = () => {
-  const editor = useEditor({
-    extensions: [
-      DocumentExtension,
-      TextExtension,
-      CustomCodeBlockExtension.configure({
-        defaultLanguage: 'markdown',
-        lowlight,
-      }),
-      Collaboration.configure({ document: doc }),
-    ],
-    editorProps: {
-      attributes: {
-        id: 'editor',
-      },
-    },
-  })
+  // TODO
+  const id = 'doc_id'
 
-  useEffect(() => {
-    console.log(editor?.getText())
-  }, [editor])
+  const _ = useMemo(() => new WebsocketProvider(`${WS.HOST}`, id, doc), [id])
+  const editorRef = useRef<PureEditorContent>(null)
+
+  const [mdBody, setMdBody] = useState<string>()
+  const onUpdate = useCallback((props: EditorEvents['update']) => {
+    setMdBody(props.editor?.getText())
+  }, [])
+
+  const editor = useEditor(
+    {
+      extensions: [
+        DocumentExtension,
+        TextExtension,
+        CustomCodeBlockExtension.configure({
+          defaultLanguage: 'markdown',
+          lowlight,
+        }),
+        Collaboration.configure({ document: doc }),
+      ],
+      editorProps: {
+        attributes: {
+          id: 'editor',
+        },
+      },
+      onUpdate,
+    },
+    []
+  )
 
   return (
-    <Container maxW="container.2xl">
-      <EditorContent editor={editor} />
-    </Container>
+    <HStack
+      h="100vh"
+      w="100vw"
+      spacing={0}
+      align="stretch"
+      divider={<StackDivider borderColor="gray.200" />}
+    >
+      <Box p={2} w="100%">
+        <Editor editor={editor} ref={editorRef} />
+      </Box>
+      <Box p={2} w="100%">
+        <Preview source={mdBody} />
+      </Box>
+    </HStack>
   )
 }
